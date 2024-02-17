320b8e8daad50f998c92ea1c28ce676782fd5d2e
Artem Averin <artem-averin-91@mail.ru>
2024-02-17 19:53:09 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/TaskStore.java
+++ src/main/java/ru/job4j/todo/repository/TaskStore.java
@@ -1,8 +1,6 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.status.Status;
@@ -13,149 +11,55 @@ import java.util.*;
 @AllArgsConstructor
 public class TaskStore implements TaskRepository {
 
-    private final SessionFactory sf;
+    private CrudRepository crudRepository;
 
     @Override
     public Task save(Task task) {
-        var session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.save(task);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.persist(task));
         return task;
     }
 
     @Override
     public boolean deleteById(int id) {
-        var session = sf.openSession();
-        int result = 0;
-        try {
-            session.beginTransaction();
-            result = session.createQuery("DELETE Task where id = :fId")
-                    .setParameter("fId", id)
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result > 0;
+        return crudRepository.booleanRun("DELETE Task where id = :fId", Map.of("fId", id));
     }
 
     @Override
     public boolean update(Task task) {
-        var session = sf.openSession();
-        int result = 0;
-        try {
-            session.beginTransaction();
-            var text = """
+        return crudRepository.booleanRun("""
                     UPDATE Task SET
                     title = :fTitle,
                     description = :fDescription
                     WHERE id = :fId
-                    """;
-            result = session.createQuery(text)
-                    .setParameter("fTitle", task.getTitle())
-                    .setParameter("fDescription", task.getDescription())
-                    .setParameter("fId", task.getId())
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result > 0;
+                    """,
+                Map.of("fTitle", task.getTitle(), "fDescription", task.getDescription(), "fId", task.getId()));
     }
 
     @Override
     public boolean complete(Task task) {
-        var session = sf.openSession();
-        int result = 0;
-        try {
-            session.beginTransaction();
-            var text = """
+        return crudRepository.booleanRun("""
                     UPDATE Task SET
                     done = :fDone 
                     WHERE id = :fId
-                    """;
-            result = session.createQuery(text)
-                    .setParameter("fDone", task.isDone())
-                    .setParameter("fId", task.getId())
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result > 0;
+                    """,
+                Map.of("fDone", task.isDone(), "fId", task.getId()));
     }
 
     @Override
     public Optional<Task> findById(int id) {
-        var session = sf.openSession();
-        Optional<Task> taskOptional = Optional.empty();
-        try {
-            session.beginTransaction();
-            Query<Task> query = session.createQuery(
-                    "from Task as i where i.id = :fId", Task.class);
-            query.setParameter("fId", id);
-            taskOptional = query.uniqueResultOptional();
-
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return taskOptional;
+        return crudRepository.optional("from Task as i where i.id = :fId", Task.class,
+                Map.of("fId", id));
     }
 
     @Override
     public Collection<Task> findAll() {
-        var session = sf.openSession();
-        List<Task> taskList = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            taskList = session.createQuery("from Task order by id", Task.class).getResultList();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return taskList;
+        return crudRepository.query("from Task order by id", Task.class);
     }
 
     @Override
     public Collection<Task> findByStatus(Status status) {
-        var session = sf.openSession();
-        List<Task> taskList = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            Query<Task> query = session.createQuery(
-                    "from Task as i where i.done = :fStatus", Task.class);
-            if (status == Status.COMPLETED) {
-                query.setParameter("fStatus", status.getStatus());
-            }
-            if (status == Status.NEW) {
-                query.setParameter("fStatus", status.getStatus());
-            }
-            taskList = query.getResultList();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return taskList;
+        return crudRepository.query("from Task as i where i.done = :fStatus",
+                Task.class, Map.of("fStatus", status.getStatus()));
     }
 
 }
--- src/main/java/ru/job4j/todo/repository/UserStore.java
+++ src/main/java/ru/job4j/todo/repository/UserStore.java
@@ -1,70 +1,32 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
 @Repository
 @AllArgsConstructor
 public class UserStore implements UserRepository {
 
-    private final SessionFactory sf;
+    private CrudRepository crudRepository;
 
     @Override
     public Optional<User> save(User user) {
-        var session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-            return Optional.of(user);
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return Optional.empty();
+        crudRepository.run(session -> session.persist(user));
+        return Optional.of(user);
     }
 
     @Override
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        var session = sf.openSession();
-        Optional<User> userOptional = Optional.empty();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery(
-                    "from User as i where i.login = :fLogin and i.password = :fPassword", User.class)
-                    .setParameter("fLogin", login)
-                    .setParameter("fPassword", password);
-            userOptional = query.uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return userOptional;
+        return crudRepository.optional("from User as i where i.login = :fLogin and i.password = :fPassword",
+                User.class,
+                Map.of("fLogin", login, "fPassword", password));
     }
 
     @Override
     public Collection<User> findAll() {
-        var session = sf.openSession();
-        List<User> taskList = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            taskList = session.createQuery("from User order by id", User.class).getResultList();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return taskList;
+        return crudRepository.query("from User order by id", User.class);
     }
 }
